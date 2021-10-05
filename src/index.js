40fb91a98805b240f87441b658a63dec3e5f7c21
Veronika <veronika.pilipenko@gmail.com>
2021-10-05 15:28:31 -0400

    making sure that the memory card is flipped

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 const coffeeCards = document.querySelectorAll('.coffeehouse-card')
 
+let flipped = false; 
+let firstCard, secondCard; 
+
 function flipCard() {
     // console.log('hello')
     this.classList.toggle('flip')
+
+    if (flipped === false) {
+        flipped = true; 
+        firstCard = this; 
+        // console.log({flipped, firstCard})
+        
+    }
 }
 
 coffeeCards.forEach(card => card.addEventListener('click', flipCard))
