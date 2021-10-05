13a93fb8e0caa9cd7406cccc2c146e51df250b77
Veronika <veronika.pilipenko@gmail.com>
2021-10-05 16:26:20 -0400

    combining everything from the new project and the styling from the old project; added modal

--- src/index.js
+++ src/index.js
@@ -68,3 +68,41 @@ function reset() {
 }
 
 coffeeCards.forEach(card => card.addEventListener('click', flipCard)); 
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    })
+})
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
+// closes the modal if you click anywhere outside of the x as well 
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+})
