9f172776d86e271f8a98dba6f25b2a0644489989
Veronika <veronika.pilipenko@gmail.com>
2021-10-05 15:43:45 -0400

    working on making the cards match up

--- src/index.js
+++ src/index.js
@@ -10,9 +10,22 @@ function flipCard() {
     if (flipped === false) {
         flipped = true; 
         firstCard = this; 
-        // console.log({flipped, firstCard})
-        
+    } else { 
+        flipped = false; 
+        secondCard = this; 
+
+        // console.log({firstCard, secondCard})
+        // console.log(firstCard.dataset.framework)
+        // console.log(secondCard.dataset.framework)
+    
+        if (firstCard.dataset.framework === secondCard.dataset.framework) {
+            firstCard.removeEventListener('click', flipCard)
+            secondCard.removeEventListener('click', flipCard)
+        }
+
+        console.log('hello')
     }
+
 }
 
 coffeeCards.forEach(card => card.addEventListener('click', flipCard))
